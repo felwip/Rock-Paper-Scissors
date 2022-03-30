80fc086f26aef82b104e13deaedb6970a2f97134
Felix Wippich <felix.wippich@me.com>
2022-03-30 12:14:30 +0200

    be: start implementing log4j

--- backend/src/main/java/com/rps/rpsbackend/RpsBackendApplication.java
+++ backend/src/main/java/com/rps/rpsbackend/RpsBackendApplication.java
@@ -6,11 +6,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 @SpringBootApplication
 public class RpsBackendApplication {
 
+	private static Logger logger = LogManager.getLogger();
+
 	public static void main(String[] args) {
 		SpringApplication.run(RpsBackendApplication.class, args);
+
+		/* ! For testing the log4j implementation. Currently not working!
+		logger.debug("It is a debug logger.");
+		logger.error("It is an error logger.");
+		logger.fatal("It is a fatal logger.");
+		logger.info("It is a info logger.");
+		logger.trace("It is a trace logger.");
+		logger.warn("It is a warn logger.");
+		 */
+
 	}
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
